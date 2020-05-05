453e4ed6e069b827c9630a91a17f719f372f4dfe
Richie Yi <richiehyi@gmail.com>
2020-05-04 21:15:32 -0700

    [wip] auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { auth } from './firebase';
 
 import LogIn from './components/LogIn';
 // import SignUp from "./components/SignUp";
@@ -6,9 +7,21 @@ import LogIn from './components/LogIn';
 import './App.css';
 
 const App = (): JSX.Element => {
+  const [user, setUser] = React.useState<any>(null);
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
+  console.log('here', user);
+
   return (
     <div className="App">
-      <LogIn />
+      {/* <LogIn /> */}
       {/* <SignUp /> */}
       {/* <Board /> */}
     </div>
