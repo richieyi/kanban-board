efdfc8dd5eeb32d4ca9481588157fb9a077ac1c9
Richie Yi <richiehyi@gmail.com>
2020-05-05 22:50:59 -0700

    [feat] app initial loading state and route home upon sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { auth } from "./firebase";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { auth } from './firebase';
 
-import LogIn from "./pages/LogIn";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
-import Board from "./components/Board";
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import Board from './components/Board';
 
 const App = (): JSX.Element => {
+  const [loading, setLoading] = React.useState<boolean>(true);
   const [user, setUser] = React.useState<any>(null);
 
   React.useEffect(() => {
     auth.onAuthStateChanged((user) => {
+      setLoading(false);
       if (user) {
         setUser(user);
       }
@@ -21,6 +23,8 @@ const App = (): JSX.Element => {
   const handleLogout = (): void => {
     auth.signOut().then(() => setUser(null));
   };
+
+  if (loading) return <div />;
 
   return (
     <Router>
