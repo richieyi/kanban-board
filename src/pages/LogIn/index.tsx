26bb27fc248c92356f679e2eb905fb37f25ed964
Richie Yi <richiehyi@gmail.com>
2020-05-08 13:24:45 -0700

    [style] log in page

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import { useHistory } from 'react-router-dom';
-import { auth } from '../../firebase';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import { auth } from "../../firebase";
+
+import styles from "./login.module.css";
+
+const useStyles = makeStyles({
+  input: {
+    marginBottom: '5px'
+  },
+  button: {
+    marginTop: '10px'
+  }
+});
 
 const LogIn = (): JSX.Element => {
-  const [email, setEmail] = React.useState<string>('');
-  const [password, setPassword] = React.useState<string>('');
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
   const history = useHistory();
+  const classes = useStyles();
 
   const handleSubmit = (
     e: React.FormEvent<HTMLButtonElement | HTMLFormElement>
@@ -15,7 +29,7 @@ const LogIn = (): JSX.Element => {
     auth
       .signInWithEmailAndPassword(email, password)
       .then((res) => {
-        history.push('/');
+        history.push("/");
         console.log(res);
       })
       .catch((err) => console.log(err));
@@ -30,15 +44,31 @@ const LogIn = (): JSX.Element => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="email" type="text" value={email} onChange={handleEmail} />
-      <input
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <TextField
+        label="Email"
+        name="email"
+        type="text"
+        value={email}
+        onChange={handleEmail}
+        className={classes.input}
+      />
+      <TextField
+        label="Password"
         name="password"
         type="password"
         value={password}
         onChange={handlePassword}
+        className={classes.input}
       />
-      <button onClick={handleSubmit}>Log In</button>
+      <Button
+        onClick={handleSubmit}
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
+        Log In
+      </Button>
     </form>
   );
 };
