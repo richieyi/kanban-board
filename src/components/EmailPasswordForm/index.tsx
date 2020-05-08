3c74f3cd278f826680b7dfe2b75c528957b9b40c
Richie Yi <richiehyi@gmail.com>
2020-05-08 13:37:55 -0700

    [clean] create reusable emailpasswordform component

---
+++ src/components/EmailPasswordForm/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
+
+import styles from "./email-password-form.module.css";
+
+const useStyles = makeStyles({
+  input: {
+    marginBottom: '5px'
+  },
+  button: {
+    marginTop: '10px'
+  }
+});
+
+interface Props {
+  email: string;
+  handleSubmit: (e: React.FormEvent<HTMLButtonElement | HTMLFormElement>) => void;
+  handleEmail: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  password: string;
+  handlePassword: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const EmailPasswordForm = (props: Props) => {
+  const { handleSubmit, email, handleEmail, password, handlePassword } = props;
+  const classes = useStyles();
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <TextField
+        label="Email"
+        name="email"
+        type="text"
+        value={email}
+        onChange={handleEmail}
+        className={classes.input}
+      />
+      <TextField
+        label="Password"
+        name="password"
+        type="password"
+        value={password}
+        onChange={handlePassword}
+        className={classes.input}
+      />
+      <Button
+        onClick={handleSubmit}
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
+        Log In
+      </Button>
+    </form>
+  );
+};
+
+export default EmailPasswordForm;
--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,26 +1,12 @@
 import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
 import { auth } from "../../firebase";
-
-import styles from "./login.module.css";
-
-const useStyles = makeStyles({
-  input: {
-    marginBottom: '5px'
-  },
-  button: {
-    marginTop: '10px'
-  }
-});
+import EmailPasswordForm from "../../components/EmailPasswordForm";
 
 const LogIn = (): JSX.Element => {
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
   const history = useHistory();
-  const classes = useStyles();
 
   const handleSubmit = (
     e: React.FormEvent<HTMLButtonElement | HTMLFormElement>
@@ -44,32 +30,13 @@ const LogIn = (): JSX.Element => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <TextField
-        label="Email"
-        name="email"
-        type="text"
-        value={email}
-        onChange={handleEmail}
-        className={classes.input}
-      />
-      <TextField
-        label="Password"
-        name="password"
-        type="password"
-        value={password}
-        onChange={handlePassword}
-        className={classes.input}
-      />
-      <Button
-        onClick={handleSubmit}
-        variant="contained"
-        color="primary"
-        className={classes.button}
-      >
-        Log In
-      </Button>
-    </form>
+    <EmailPasswordForm
+      handleSubmit={handleSubmit}
+      email={email}
+      handleEmail={handleEmail}
+      password={password}
+      handlePassword={handlePassword}
+    />
   );
 };
 
